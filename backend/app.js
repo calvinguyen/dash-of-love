9a5600ef3f3f6af470507dd582384227b46ecadc
calvinguyen <77700030+calvinguyen@users.noreply.github.com>
2023-03-17 15:05:23 -0500

    Setup initial routes and controllers for approved erd tables

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,12 @@ import morgan from "morgan"
 //Import routes
 import testsRoute from "./routes/tests.js"
 import flavorsRoute from "./routes/flavors.js"
+import customersRoute from "./routes/customers.js"
+import invoicesRoute from "./routes/invoices.js"
+import menuRoute from "./routes/menu.js"
+import referralsRoute from "./routes/referrals.js"
+import requestsRoute from "./routes/requests.js"
+import sizesRoute from "./routes/sizes.js"
 
 dotenv.config()
 const PORT = process.env.PORT || 3000;
@@ -24,6 +30,12 @@ app.use(morgan("dev"))
 //Setup middleware for routes
 app.use("/tests", testsRoute)
 app.use("/flavors", flavorsRoute)
+app.use("/customers", customersRoute)
+app.use("/invoices", invoicesRoute)
+app.use("/menu", menuRoute)
+app.use("/referrals", referralsRoute)
+app.use("/requests", requestsRoute)
+app.use("/sizes", sizesRoute)
 
 app.listen(PORT, () => {
     console.log("Server started listening on port: ", PORT);
---
+++ backend/routes/sizes.js
@@ -0,0 +1,10 @@
+import express from "express"
+/* import controller functions from ../controllers/fileName.js
+-- check flavors.js for example 
+*/
+
+const router = express.Router()
+
+
+
+export default router
