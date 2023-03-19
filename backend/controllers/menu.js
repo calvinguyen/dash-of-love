d8ccddffa088dca06ba270f00a86d4f4495ad22a
calvinguyen <77700030+calvinguyen@users.noreply.github.com>
2023-03-19 15:06:51 -0500

    Implement all working routes and controllers for menu

--- backend/controllers/menu.js
+++ backend/controllers/menu.js
@@ -1,1 +1,65 @@
 import {db} from "../database.js"
+
+// Get all Menu cake flavors
+export async function getMenu() {
+    const sql = "select * from Menu";
+    const [results] = await db.query(sql);
+  
+    return results
+}
+
+// Get specific cake type and flavor by id on menu 
+export async function getMenuItemById(id) {
+    const sql = `
+      select *
+      from Menu
+      where cakesID = ?
+    `;
+    const [rows] = await db.query(sql, [id]);
+    return rows[0];
+}
+
+// Get all flavors offered for a specific cake type by typeid
+export async function getFlavorsOfferedByCakeType(typeID) {
+    const sql = `
+      select *
+      from Menu
+      where typeID = ?
+    `;
+    const [rows] = await db.query(sql, [typeID]);
+    return rows;
+}
+
+// Get all cake types offered for a specific flavor by flavorid
+export async function getCakeTypesOfferedByFlavor(flavorID) {
+    const sql = `
+      select *
+      from Menu
+      where flavorID = ?
+    `;
+    const [rows] = await db.query(sql, [flavorID]);
+    return rows;
+}
+
+// Assign a flavor to cake type
+export async function assignFlavorToCake(typeID, flavorID) {
+    const sql = `
+      insert into Menu (typeID, flavorID)
+      values (?, ?)
+    `;
+    const [result] = await db.query(sql, [typeID, flavorID]);
+    const id = result.insertId;
+  
+    return getMenuItemById(id);
+}
+
+// Delete a cake type to flavor assignment
+export async function removeCakeFlavorAssignment(cakesID) {
+    const sql = `
+      delete from Menu
+      where cakesID = ?
+    `;
+    const [result] = await db.query(sql, [cakesID]);
+  
+    return result;
+}
--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -1,10 +1,58 @@
 import express from "express"
-/* import controller functions from ../controllers/fileName.js
--- check flavors.js for example 
-*/
+import {
+    getMenu, getMenuItemById, getFlavorsOfferedByCakeType, getCakeTypesOfferedByFlavor, assignFlavorToCake, removeCakeFlavorAssignment
+} from "../controllers/menu.js"
 
 const router = express.Router()
 
+// Get all rows from menu 
+router.get("/", async (req, res) => {
+    const data = await getMenu();
 
+    res.json(data);
+})
+
+// Get Menu Item from Menu by id
+router.get("/:id", async (req, res) => {
+    const cakesId = req.params.id;
+    const data = await getMenuItemById(cakesId);
+
+    res.json(data);
+})
+
+// Get all Flavors offered for a specific cake type by typeID
+router.get("/cake-type/flavors/:id", async (req, res) => {
+    const typeId = req.params.id;
+    const data = await getFlavorsOfferedByCakeType(typeId);
+
+    res.json(data);
+})
+
+// Get all Cake-Types offered for a specific flavor by flavorID
+router.get("/flavor/cake-types/:id", async (req, res) => {
+    const flavorId = req.params.id;
+    const data = await getCakeTypesOfferedByFlavor(flavorId);
+
+    res.json(data);
+})
+
+// Assign a flavor to cake type
+router.post("/", async (req, res) => {
+    const typeId = req.body.typeID;
+    const flavorId = req.body.flavorID;
+    
+    const data = await assignFlavorToCake(typeId, flavorId);
+    res.json(data);
+})
+
+// Remove a cake-type to flavor assignment by cakesID
+router.delete("/:id", async (req, res) => {
+    const cakesId = req.params.id;
+
+    removeCakeFlavorAssignment(cakesId)
+        .then(result => {
+            res.json("Successfully deleted a menu item");
+        })
+})
 
 export default router
